af63ad08faab8eb9ad21e33c3b9b0a01f8397a54
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-09 14:16:51 -0300

    Add create User Controller with Validations

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -2,3 +2,9 @@ export interface HttpResponse<T> { //<T> Genérico
     statusCode: number,
     body: T | string
 }
+
+export interface HttpRequest<B> {
+    params?: B;
+    header?: B;
+    body?: B;
+}
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express from 'express';
 import { GetUsersControllers } from './controllers/get-users/get-users';
 import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-users';
 import { MongoClient } from './database/mongo';
+import { MongoCreateUserRepository } from './repositories/create-user/mongo-create-user';
+import { CreateUserController } from './controllers/create-user/create-user';
 
 const main = async () => {
     const app = express();
     await MongoClient.connect();
+
+    app.use(express.json());
 
     app.get("/users", async  (req, res) => {
         const mongoGetUsersRepository = new MongoGetUsersRepository();
@@ -16,6 +20,16 @@ const main = async () => {
     
         res.send(response.body).status(response.statusCode);
     });
+
+    app.post('/users', async (req, res) => {
+        const mongoCreateUserRepository = new MongoCreateUserRepository();
+
+        const createUserController = new CreateUserController(mongoCreateUserRepository);
+
+        const response = await createUserController.handle({body: req.body});
+
+        res.send(response.body).status(response.statusCode);
+    })
     
     const port = process.env.PORT;
 
