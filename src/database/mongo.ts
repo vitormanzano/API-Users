10ebb584784c127ef92aa15c5662c9a7c2491eb5
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-09 21:46:04 -0300

    OK

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,15 +1,22 @@
-import {MongoClient as Mongo, Db} from 'mongodb'
+import { MongoClient as Mongo, Db, ServerApiVersion } from 'mongodb'
 
 export const MongoClient = {
     client: undefined as unknown as Mongo,
     db: undefined as unknown as Db,
 
     async connect(): Promise<void> {
-        const url = process.env.MONGODB_URL ||  "mongodb+srv://root:123@cluster0.kfffe.mongodb.net";
+        const url = process.env.MONGODB_URL!;
         const username = process.env.MONGODB_USERNAME
         const password = process.env.MONGODB_PASSWORD
 
-        const client = new Mongo(url, {auth: {username, password}});
+        const client = new Mongo(url,
+            {
+                serverApi: {
+                    version: ServerApiVersion.v1,
+                    strict: true,
+                    deprecationErrors: true,
+                }
+            });
         const db = client.db("users-db")
 
         this.client = client;
