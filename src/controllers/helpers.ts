27b6ca287adc8a5ff0ab1e8c6f58edb090fcaefe
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-09 21:21:21 -0300

    Interpolação string

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -31,6 +31,6 @@ export const serverError = (): HttpResponse<string> => {
 export const serverErrorString = (message: string): HttpResponse<string> => {
     return {
         statusCode: HttpStatusCode.SERVER_ERROR,
-        body: "Something went wrong... ",
+        body: `Something went wrong...  ${message}`,
     };
 };
