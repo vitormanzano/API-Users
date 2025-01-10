1108b4603af58329cc49c415571d1e8d3e36c447
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-09 21:00:59 -0300

    Revert "Testando"
    This reverts commit aa7987d32358245b020e42754d258cf28d4ccc86.

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -21,7 +21,7 @@ export const created = <T>(body: any): HttpResponse<T> => {
     };
 };
 
-export const serverError = (message: Error): HttpResponse<string> => {
+export const serverError = (): HttpResponse<string> => {
     return {
         statusCode: HttpStatusCode.SERVER_ERROR,
         body: "Something went wrong... ",
