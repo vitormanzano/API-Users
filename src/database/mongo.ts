9df7a967487cb22323fd544053811440c4f485d8
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-01-10 11:06:56 -0300

    Refactoring .env with mongo

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,11 +1,15 @@
 import { MongoClient as Mongo, Db, ServerApiVersion } from 'mongodb'
 
+require('dotenv').config();
+
+
 export const MongoClient = {
     client: undefined as unknown as Mongo,
     db: undefined as unknown as Db,
 
     async connect(): Promise<void> {
-        const url = "mongodb+srv://root:123@cluster0.kfffe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //process.env.MONGODB_URL!;
+        console.log(process.env.MONGODB_URL);
+        const url = process.env.MONGODB_URL!;
         const username = process.env.MONGODB_USERNAME
         const password = process.env.MONGODB_PASSWORD
 
